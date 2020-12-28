27e12875f82c213dbd8064bb43c9a642c0cb2ac8
VENKATREDDDY MALLIDI <venkatreddymallidi99999@gmail.com>
2020-12-28 14:05:42 +0530

    checking factors other than 1 and itself

---
+++ prime1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class prime
+{
+public static void main(String args[])
+{
+int count=0;
+Scanner input=new Scanner(System.in);
+System.out.println("Enter a number:");
+int no=input.nextInt();
+if(no==1 || no==0)
+{
+System.out.println(no+" not a prime number");
+}
+else
+{
+for(int i=2;i<no;i++)
+{
+if(no%i==0)
+{
+count++;
+break;
+}
+}
+if(count==0)
+{
+System.out.println("prime number");
+}
+else 
+{
+System.out.println("not a prime number"); 
+}
+}
+}
+}
