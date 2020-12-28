3a577be9a83ada5e15db974c21bc8809ab329e15
VENKATREDDDY MALLIDI <venkatreddymallidi99999@gmail.com>
2020-12-28 13:29:11 +0530

    two types of comments

---
+++ commenttypes.java
@@ -0,0 +1,13 @@
+class comments
+{
+public static void main(String args[])
+{
+/*the 
+programs
+prints
+user given text*/
+System.out.println("multiline comments:(/*....*/)");
+// print for output in same line but println for output new line
+System.out.println("single line comments:(//)");
+}
+}
