1999f05f42dddddbff95f77b79cbe9320a5bbb02
VENKATREDDDY MALLIDI <venkatreddymallidi99999@gmail.com>
2021-01-01 19:24:47 +0530

    /n and /t usage

---
+++ escapesequences.java
@@ -0,0 +1,17 @@
+// "\n" and "\t"
+class Newline
+{
+public static void main(String args[])
+{
+System.out.println("numbers in separate line:");
+for(int i=1;i<5;i++)
+{
+System.out.println(i);  //println=printinline
+}
+System.out.println("numbers with tab space:");
+for(int j=1;j<5;j++)
+{
+System.out.print(j+" "); //print=print in same line and empty space quotes for tabspace
+}
+}
+}
