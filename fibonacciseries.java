f97b2bb9699e2c6007cf08941ea82786eed52ba4
VENKATREDDDY MALLIDI <venkatreddymallidi99999@gmail.com>
2021-01-03 20:32:09 +0530

    fibonacci series upto n terms

--- fibonacciseries.java
+++ fibonacciseries.java
@@ -4,16 +4,16 @@ class series
 public static void main(String args[])
 {
 Scanner input=new Scanner(System.in);
-System.out.println("Enter no of term of the series");
-int r=input.nextInt();
-System.out.println("fibonacci series upto "+r+" terms:");
+System.out.println("Enter no of terms of the series");
+int t=input.nextInt();
+System.out.println("fibonacci series upto "+t+" terms are:");
 int a=0,b=1;
 System.out.print(a+" "+b+" ");
 int c=a+b;
-for(int i=3;i<=r;i++)
+for(int i=3;i<=t;i++)  //i value from 3 because we already printed 2 terms
 {
-System.out.print(c+" ");
-a=b;
+System.out.print(c+" "); // empty quotes for tab space
+a=b;  //swaping a,b values
 b=c;
 c=a+b;
 }
