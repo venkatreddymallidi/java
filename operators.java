f416eaf0d112fac111453786d83f9d401dba91f9
VENKATREDDDY MALLIDI <venkatreddymallidi99999@gmail.com>
2021-02-10 14:31:15 +0530

    arithmetic operators

---
+++ operators.java
@@ -0,0 +1,15 @@
+class arithmeticoperator
+{
+public static void main(String args[])
+{
+int n1=100;
+int n2=10;
+System.out.println("addition operator "+n1+"+"+n2+"="+(n1+n2));
+System.out.println("subtraction operator "+n1+"-"+n2+"="+(n1-n2));
+System.out.println("mutliplication operator "+n1+"*"+n2+"="+(n1*n2));
+System.out.println("division operator "+n1+"/"+n2+"="+(n1/n2));
+System.out.println("remainder or modulus operator "+n1+"%"+n2+"="+(n1%n2));
+}
+}
+
+
