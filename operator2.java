c3cbb3458a6309e96116239b743796bb00de7e8a
VENKATREDDDY MALLIDI <venkatreddymallidi99999@gmail.com>
2021-02-16 13:46:46 +0530

    logical operator

---
+++ operator2.java
@@ -0,0 +1,15 @@
+/*logical operator mainly used in conditional and looping statements*/
+class logicaloperator 
+{
+public static void main(String args[])
+{
+System.out.println("true && true ="+(true&&true)); //&& -logical and
+System.out.println("true && false ="+(true&&false));
+System.out.println("false && false ="+(false&&false));
+System.out.println("true || true ="+(true || true)); // || -logical or
+System.out.println("true || false ="+(true||false));
+System.out.println("false || false ="+(false||false));
+System.out.println("! true ="+(!true));
+System.out.println("! false ="+(!false));
+}
+}
